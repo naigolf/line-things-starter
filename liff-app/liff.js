782049eb21d8638d49c4562a9b1e5797b7d135a0
nai golf <niy_golf@hotmail.com>
2019-07-24 23:50:13 +0700

    Update liff.js

--- liff-app/liff.js
+++ liff-app/liff.js
@@ -1,5 +1,5 @@
 // User service UUID: Change this to your generated service UUID
-const USER_SERVICE_UUID         = '1346864a-3b6a-4aa0-96d8-dbc96ae20870'; // LED, Button
+const USER_SERVICE_UUID         = 'a3de6f4b-4625-4f94-8374-bbd3c719a0d0'; // LED, Button
 // User service characteristics
 const LED_CHARACTERISTIC_UUID   = 'E9062E71-9E62-4BC6-B0D3-35CDCD9B027B';
 const BTN_CHARACTERISTIC_UUID   = '62FBD229-6EDD-4D1A-B554-5C4E1BB29169';
